f71bf0fb8779d887edb22c6fb06246c2079f31e6
rapidbnnuy <rapidbnnuy@rapidbnnuy.local>
2025-12-31 10:44:54 -0500

    Initial commit: Streamer.bot plugin setup

---
+++ PNGTuber-GPTv2/CPHInline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Streamer.bot.Plugin.Interface;
+
+namespace PNGTuber_GPTv2
+{
+    public class CPHInline
+    {
+        public IInlineInvokeProxy CPH { get; set; }
+
+        public void Execute()
+        {
+            CPH.LogInfo("PNGTuber-GPTv2 Plugin Initialized!");
+        }
+    }
+}
