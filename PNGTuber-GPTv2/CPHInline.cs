d352e0fed5763d11e00972f6f477cf6be3622a6e
rapidbnnuy <rapidbnnuy@rapidbnnuy.local>
2025-12-31 11:18:30 -0500

    Implement dependency usage in CPHInline.cs

--- PNGTuber-GPTv2/CPHInline.cs
+++ PNGTuber-GPTv2/CPHInline.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Channels;
+using LiteDB;
 using Streamer.bot.Plugin.Interface;
 
 namespace PNGTuber_GPTv2
@@ -11,6 +13,25 @@ namespace PNGTuber_GPTv2
         public void Execute()
         {
             CPH.LogInfo("PNGTuber-GPTv2 Plugin Initialized!");
+
+            // Verify System.Threading.Channels
+            var channel = Channel.CreateUnbounded<string>();
+            CPH.LogInfo($"Channel created with capacity: {channel.Reader.CanCount}");
+
+            // Verify LiteDB
+            try 
+            {
+                using(var db = new LiteDatabase(":memory:"))
+                {
+                    var col = db.GetCollection<BsonDocument>("test");
+                    col.Insert(new BsonDocument { ["msg"] = "LiteDB Works!" });
+                    CPH.LogInfo($"LiteDB Insert count: {col.Count()}");
+                }
+            }
+            catch(Exception ex)
+            {
+                CPH.LogInfo($"LiteDB Error: {ex.Message}");
+            }
         }
     }
 }
